c3bb2132dc1fb907391415d21903419e1b11055c
[magnetogithub] <[jwlim011@gmail.com]>
2025-04-28 16:31:18 +0900

    First commit: Upload Spring Boot Project

---
+++ src/main/java/com/Gym_PT/Gym_PT/GymPtApplication.java
@@ -0,0 +1,13 @@
+package com.Gym_PT.Gym_PT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymPtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymPtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Gym_PT/Gym_PT/GymPtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gym_PT.Gym_PT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymPtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
